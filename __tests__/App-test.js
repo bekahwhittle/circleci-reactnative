7d1cf945e6cd2038df37d6884dbe93d840998627
Rebekah Whittle <whittlebekah@gmail.com>
2020-08-11 12:43:38 -0500

    ✅ test additional

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -7,6 +7,7 @@ import React from 'react';
 import App from '../App';
 
 // Note: test renderer must be required after react-native.
+// Test pass or fail
 import renderer from 'react-test-renderer';
 
 it('renders correctly', () => {
